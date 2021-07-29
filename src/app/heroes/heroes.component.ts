866e4afbf16fa3e487adc40c8991f7dadce3371d
lokesh2096 <lokeshkumar2096@gmail.com>
2021-07-29 18:12:30 +0530

    29-07-2021

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { throwError } from 'rxjs';
+import { Subscriber, Subscription, throwError } from 'rxjs';
 import { Observable } from 'rxjs';
 import { SharedService } from '../sharedServices/shared.service';
 import { map, catchError } from 'rxjs/operators';
@@ -12,17 +12,21 @@ import { UserListurl } from '../config';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute,private shared:SharedService) { }
+  constructor(private activatedRoute: ActivatedRoute,private shared:SharedService) { 
+  }
   orders:any;
+  orders$:Observable<any[]>;
+  orders2:Subscription;
   ngOnInit(): void {
-  //   this.shared.getOrderRequest().pipe().subscribe((res)=>{
-  //     this.orders =res;
-  //    },
-  //    (err)=>{
-  //    console.log(err)
-  //    }
-     
-  //  )
+    this.orders$ = this.shared.getOrderRequest();
+    this.getOrders();
+ this.orders2 = this.shared.getOrderRequest().subscribe((res)=>{
+      this.orders =res;
+      console.log(res);
+     },
+     (err)=>{
+     console.log(err)
+     } )
     // this.shared.Get(UserListurl).then(
     //   (data)=>{
     //     this.orders =data;
@@ -32,7 +36,7 @@ export class HeroesComponent implements OnInit {
     //     this.orders =data;
     //   }
     // );
-   this.orders =  this.activatedRoute.snapshot.data['order'];
+   //this.orders =  this.activatedRoute.snapshot.data['order'];
   //  this.orders.map((data)=>{
   //    data['keyname'] = 'ANONYMOUS';
   //    data['email'] = 'lokeshkumar2096@gmail.com';
@@ -40,8 +44,35 @@ export class HeroesComponent implements OnInit {
   //  })
   //  console.log(this.orders);
   }
+
+async getOrders(){
+    // this.shared.Get(UserListurl).then(
+    //     (data)=>{
+    //       this.orders =data;
+
+          
+    //     }
+    //   ).catch(
+    //     (data)=>{
+    //       this.orders =data;
+    //     }
+    //   );
+    this.orders = await this.shared.Get(UserListurl);
+
+    console.log(this.orders);
+  }
+
+  openComponetAgain(){
+    this.shared.openFormComponent();
+  }
+  closeModal(){
+    this.shared.closeModal();
+  }
+
   ngOnDestroy(){
-    console.log('DESTROYED')
+    if(this.orders2){
+       this.orders2.unsubscribe();
+    }
   }
 
 }
